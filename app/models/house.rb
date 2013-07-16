9c90d760d279c2498e5474b1a1928e4b85b6f2c0
Surabhi <surabhi.rammohan@strata.co.in>
2013-07-16 15:17:29 +0530

    Create Person model with specs

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,8 @@
 class House < ActiveRecord::Base
   attr_accessible :address, :num_of_ppl
+
+  validates :address, presence: true
+
+  validates :num_of_ppl, presence: true, :inclusion => { :in => 0..100 }
+  
 end
--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe House do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  before do
+      @house = House.new(address: "RT nagar, bangalore-32", num_of_ppl: "4")
+    end 
+  subject { @house }
+
+  it{ should be_valid }
+
+  describe "address should not be empty" do
+  	before{ @house.address = " " }
+    it { should_not be_valid}
+  end
+
+  describe "num_of_ppl should not be empty" do
+  	before{ @house.num_of_ppl = nil}
+  	it{ should_not be_valid}
+  end
+
+  describe "num_of_ppl must not be negative" do
+  	before{ @house.num_of_ppl = "-1" }
+  	it{ should_not be_valid}
+  end
+
+  describe "num_of_ppl can be zero" do
+  	before{ @house.num_of_ppl = "0" }
+  	it{ should be_valid } 
+  end
+
 end
